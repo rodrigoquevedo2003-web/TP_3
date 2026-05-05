96eb1201194ca058360e0f96624173712fdd839a
gonzalonavalesi <gonzalonavalesi@gmail.com>
2026-05-04 23:13:21 -0300

    agrego UsuarioDAO

---
+++ src/main/java/com/finanzas/personales/dao/UsuarioDAO.java
@@ -0,0 +1,28 @@
+package com.finanzas.personales.dao;
+
+import com.finanzas.personales.model.Usuario;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UsuarioDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UsuarioDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // INSERTAR USUARIO
+    public void guardar(Usuario usuario) {
+        String sql = "INSERT INTO usuarios (nombre, apellido, username, email, password) VALUES (?, ?, ?, ?, ?)";
+
+        jdbcTemplate.update(sql,
+                usuario.getNombre(),
+                usuario.getApellido(),
+                usuario.getUsername(),
+                usuario.getEmail(),
+                usuario.getPassword()
+        );
+    }
+}
