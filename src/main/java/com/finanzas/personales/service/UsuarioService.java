a33c7df3331dcb093590e44fb819f0c5f2ccc99a
gonzalonavalesi <gonzalonavalesi@gmail.com>
2026-05-04 23:22:20 -0300

    agrego Usuario Service

---
+++ src/main/java/com/finanzas/personales/service/UsuarioService.java
@@ -0,0 +1,19 @@
+package com.finanzas.personales.service;
+
+import com.finanzas.personales.dao.UsuarioDAO;
+import com.finanzas.personales.model.Usuario;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioDAO usuarioDAO;
+
+    public UsuarioService(UsuarioDAO usuarioDAO) {
+        this.usuarioDAO = usuarioDAO;
+    }
+
+    public void crearUsuario(Usuario usuario) {
+        usuarioDAO.guardar(usuario);
+    }
+}  
