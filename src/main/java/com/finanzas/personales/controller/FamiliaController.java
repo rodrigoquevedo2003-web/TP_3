fcc34039a97bb6a31d12217d737e6559b1aa5cc4
JulianRamos4606 <121529599+JulianRamos4606@users.noreply.github.com>
2026-05-05 16:46:00 -0300

    Se agrego FamilliaController

---
+++ src/main/java/com/finanzas/personales/controller/FamiliaController.java
@@ -0,0 +1,22 @@
+package com.finanzas.personales.controller;
+
+import com.finanzas.personales.model.Familia;
+import com.finanzas.personales.service.FamiliaService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/familias")
+public class FamiliaController {
+
+    private final FamiliaService familiaService;
+
+    public FamiliaController(FamiliaService familiaService) {
+        this.familiaService = familiaService;
+    }
+
+    @PostMapping
+    public String crearFamilia(@RequestBody Familia familia) {
+        familiaService.crearFamilia(familia);
+        return "Familia creada correctamente";
+    }
+}
