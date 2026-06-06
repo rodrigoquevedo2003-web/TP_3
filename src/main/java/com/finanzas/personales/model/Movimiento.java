eb600a52f58094393de707bba284b3fc81b403a0
thomyyledesma <t.ledesma2006@gmail.com>
2026-06-06 17:24:28 -0300

    Corregido model de movimiento

--- src/main/java/com/finanzas/personales/model/Movimiento.java
+++ src/main/java/com/finanzas/personales/model/Movimiento.java
@@ -1,11 +1,20 @@
 package com.finanzas.personales.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.finanzas.personales.enums.TipoMovimiento;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="movimiento")
 public class Movimiento {
@@ -16,12 +25,15 @@ public class Movimiento {
 
     @ManyToOne
     @JoinColumn(name = "cuenta_id", nullable = false)
+    @JsonIgnoreProperties({"movimientos", "usuario"})
     private Cuenta cuenta;
 
     @ManyToOne
     @JoinColumn(name = "categoria_id", nullable = false)
+    @JsonIgnoreProperties({"movimientos", "usuario"})
     private Categoria categoria;
 
+     @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private TipoMovimiento tipo;
@@ -29,91 +41,11 @@ public class Movimiento {
     @Column(nullable = false)
     private String descripcion;
 
+    @NotNull
+    @DecimalMin(value = "0.01", message = "El monto debe ser mayor a cero")
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal monto;
 
     @Column(nullable = false)
     private LocalDate fecha;
-
-    @Column(nullable = false)
-    private Boolean esFamiliar;
-
-    public Movimiento() {
-    }
-
-    public Movimiento(Long id, Cuenta cuenta, Categoria categoria, TipoMovimiento tipo, String descripcion, BigDecimal monto, LocalDate fecha, Boolean esFamiliar) {
-        this.id = id;
-        this.cuenta = cuenta;
-        this.categoria = categoria;
-        this.tipo = tipo;
-        this.descripcion = descripcion;
-        this.monto = monto;
-        this.fecha = fecha;
-        this.esFamiliar = esFamiliar;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-    public Cuenta getCuenta() {
-        return cuenta;
-    }
-
-    public void setCuenta(Cuenta cuenta) {
-        this.cuenta = cuenta;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
-
-    public TipoMovimiento getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoMovimiento tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public BigDecimal getMonto() {
-        return monto;
-    }
-
-    public void setMonto(BigDecimal monto) {
-        this.monto = monto;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-
-    public Boolean getEsFamiliar() {
-        return esFamiliar;
-    }
-
-    public void setEsFamiliar(Boolean esFamiliar) {
-        this.esFamiliar = esFamiliar;
-    }
 }
