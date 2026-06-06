fd710b6d6a3fc558e08945b385555858d13f744a
rodrigoquevedo2003-web <rodrigoquevedo2003@gmail.com>
2026-06-06 15:55:00 -0300

    mejora de seguridad de movimientos por usuario

--- src/main/java/com/finanzas/personales/repository/MovimientoRepository.java
+++ src/main/java/com/finanzas/personales/repository/MovimientoRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface MovimientoRepository extends JpaRepository<Movimiento, Long> {
 
     List<Movimiento> findByCuentaId(Long cuentaId);
-
-    List<Movimiento> findByCategoriaId(Long categoriaId);
-
+    List<Movimiento> findByCuentaUsuarioId(Long usuarioId);
+    List<Movimiento> findByCategoriaIdAndCuentaUsuarioId(Long categoriaId,Long usuarioId);
 
 }
