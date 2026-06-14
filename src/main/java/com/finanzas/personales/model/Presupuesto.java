c27a1a160a24f976cdf8fcf8cf7b37b6c183d879
thomyyledesma <t.ledesma2006@gmail.com>
2026-06-14 04:08:37 -0300

    Se agrego lombok para el model de presupuesto con data y constructores

--- src/main/java/com/finanzas/personales/model/Presupuesto.java
+++ src/main/java/com/finanzas/personales/model/Presupuesto.java
@@ -2,8 +2,15 @@ package com.finanzas.personales.model;
 
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "presupuesto")
 public class Presupuesto {
@@ -32,72 +39,4 @@ public class Presupuesto {
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
-    public Presupuesto(Long id, BigDecimal montoLimite, BigDecimal montoConsumido, int mes, int anio, Usuario usuario, Categoria categoria) {
-        this.id = id;
-        this.montoLimite = montoLimite;
-        this.montoConsumido = montoConsumido;
-        this.mes = mes;
-        this.anio = anio;
-        this.usuario = usuario;
-        this.categoria = categoria;
-    }
-
-    public Presupuesto() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public BigDecimal getMontoLimite() {
-        return montoLimite;
-    }
-
-    public void setMontoLimite(BigDecimal montoLimite) {
-        this.montoLimite = montoLimite;
-    }
-
-    public BigDecimal getMontoConsumido() {
-        return montoConsumido;
-    }
-
-    public void setMontoConsumido(BigDecimal montoConsumido) {
-        this.montoConsumido = montoConsumido;
-    }
-
-    public int getMes() {
-        return mes;
-    }
-
-    public void setMes(int mes) {
-        this.mes = mes;
-    }
-
-    public int getAnio() {
-        return anio;
-    }
-
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
-}
+   }
