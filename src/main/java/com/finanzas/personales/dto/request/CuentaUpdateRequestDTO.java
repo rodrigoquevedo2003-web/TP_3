6aa52ff082dc39887087430a3e6e32e42c37d5bb
thomyyledesma <t.ledesma2006@gmail.com>
2026-06-15 13:22:46 -0300

    Not null a tipo cuenta para que el cliente envie un tipo valido

--- src/main/java/com/finanzas/personales/dto/request/CuentaUpdateRequestDTO.java
+++ src/main/java/com/finanzas/personales/dto/request/CuentaUpdateRequestDTO.java
@@ -2,6 +2,7 @@ package com.finanzas.personales.dto.request;
 
 import com.finanzas.personales.enums.TipoCuenta;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -10,5 +11,6 @@ public class CuentaUpdateRequestDTO {
     @NotBlank(message = "El nombre de la cuenta es obligatorio")
     private String nombre;
 
+    @NotNull(message = "El tipo de cuenta es obligatorio")
     private TipoCuenta tipoCuenta;
 }
